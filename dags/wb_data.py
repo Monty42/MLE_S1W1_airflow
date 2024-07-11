3ba12ce4bbf1b5cc8a8298f7e38ba9351f3498a1
Monty42 <vas.iwan2010@gmail.com>
2024-07-11 08:28:41 +0000

    fix exercise

--- dags/wb_data.py
+++ dags/wb_data.py
@@ -138,133 +138,23 @@ def prepare_wb_data():
 
     @task()
     def transform(data: Dict) -> pd.DataFrame:
-        logging.info(LOG_FORMAT + 'Start the transform part')
-        def transform_projects(
-                df: pd.DataFrame, 
-                country_mapping: Dict,
-                non_countries: List[str],
-            ) -> pd.DataFrame:
-            logging.info(LOG_FORMAT +  'Transform projects dataset')
-            # убираем лишнее в названии страны
-            df['countryname'] = df['countryname'].str.split(';').str.get(0)
-            # подготовливаем данные для поля `countrycode`
-            project_country_abbrev_dict = defaultdict(str)
-            for country in df['countryname'].drop_duplicates().sort_values():
-                try:
-                    project_country_abbrev_dict[country] = countries.lookup(country).alpha_3
-                except: 
-                    continue
-            project_country_abbrev_dict.update(country_mapping)
-            # добавляем столбец `countrycode` в датафрейм
-            df['countrycode'] = df['countryname'].apply(lambda x: project_country_abbrev_dict[x])
-            # работаем с датами, выбираем только год
-            df['boardapprovaldate'] = pd.to_datetime(df['boardapprovaldate'])
-            df['closingdate'] = pd.to_datetime(df['closingdate'])
-            df['year'] = df['boardapprovaldate'].dt.year
-            df['year_close'] = df['closingdate'].dt.year
-            # заполняем пропуски для того, чтобы удалить элементы, у которых невозможно найти `countrycode`
-            df.fillna('', inplace=True)
-            df = df[df.countrycode != '']
-            # создаем поле `target` на основе года закрытия
-            df['target'] = df.year_close.apply(lambda x: 1 if x != '' else 0)
-            # преобразуем столбцы с датами к формату строки (иначе постгрес будет ругаться)
-            df['year'] = df['year'].astype(str).str.slice(stop=4)
-            df['year_close'] = df['year_close'].astype(str).str.slice(stop=4)
-            # чистим поле `sector1` от лишних символов
-            df['sector1'] = df['sector1'].replace('!$!0', np.nan)
-            df['sector1'] = df['sector1'].replace('!.+', '', regex=True)
-            df['sector1'] = df['sector1'].replace('^(\(Historic\))', '', regex=True)
-            # оставляем в датафрейме только страны
-            df = df[~df['countryname'].isin(non_countries)]
-            # выбираем только нужные столбцы
-            df = df[['id', 'countryname', 'sector1', 'countrycode', 'totalamt', 'year', 'year_close', 'target']]
-            return df
-        
-        def transform_other(df: pd.DataFrame, non_countries: List[str], target_column: str)  -> pd.DataFrame:
-            # удаляем информацию об индикаторох, так как в одной таблице присутствует только один индикатор
-            df.drop(columns=['Indicator Name', 'Indicator Code'], inplace=True)
-            df.drop_duplicates(subset=['Country Name', 'Country Code'], inplace=True)
-            # мелтим таблицу, чтобы получить ее вертикальную версию
-            df_melt = df.melt(
-                id_vars=['Country Name', 'Country Code'],
-                var_name='year',
-                value_name=f'{target_column}'
-            )
-            # в полученном датафрейме заполняем пустые значения
-            df_melt[f'{target_column}'] = (
-                df_melt.sort_values('year')
-                        .groupby(['Country Name', 'Country Code'])[f'{target_column}']
-                        .fillna(method='ffill')
-                        .fillna(method='bfill')
-            )
-            # чистим поле страны от регионов
-            df_melt = df_melt[~df_melt['Country Name'].isin(non_countries)]
-            # переводим столбец индикатора к числовому типу
-            df_melt[f'{target_column}'] = df_melt[f'{target_column}'].astype(float)
-            return df_melt
-        logging.info(LOG_FORMAT + 'Collect data from extract')
-        logging.info(LOG_FORMAT + f'{type(data)}')
-        logging.info(LOG_FORMAT + 'Transform projects')
-        # работаем с файлом проектов
-        df_project = transform_projects(data['df_project'], data['country_mapping'], data['non_countries'])
-        logging.info(LOG_FORMAT + 'Transform other')
-        # собираем общий файл с экономическими индикаторами
-        df_vvp = transform_other(data['df_vvp'], data['non_countries'], 'vvp')
-        df_population  = transform_other(data['df_population'], data['non_countries'], 'population')
-        df_indicator = df_vvp.merge(
-            df_population,
-            on=('Country Name', 'Country Code', 'year'),
-        )
-        # оставляем только нужные столбца
-        df_indicator.columns = ['countryname', 'countrycode', 'year', 'vvp', 'population']
-        logging.info(LOG_FORMAT + f'Number of clear data -- {df_indicator.countrycode.isna().sum()}')
-        logging.info(LOG_FORMAT + f'Number of clear data -- {df_project.countrycode.isna().sum()}')
-        logging.info(LOG_FORMAT + 'Merging data')
-        # джоиним датафрейм проектов и датафрейм индикаторов
-        df_project_meta = df_project.merge(
-            df_indicator,
-            on=('countrycode', 'year'),
-            how='left',
-        )
-        # переименуем столбцы, так как при джоине были одинаковые колонки, но с разными значениями
-        df_project_meta.rename(
-            columns={
-                'countryname_x': 'countryname_off',
-                'countryname_y': 'countryname',
-            },
-            inplace=True,
-        )
-        df_project_meta.reset_index(drop=True, inplace=True)
-        # переименуем поле `id`
-        df_project_meta.rename(columns={'id': 'project_id'}, inplace=True)
-        logging.info(LOG_FORMAT + f'Size of final data {df_project_meta.shape}')
-        logging.info(LOG_FORMAT + 'Push data from transform part to task_instance')
-        logging.info(LOG_FORMAT + 'End of transform part')
-        return df_project_meta
+        # Your code here
+        pass
 
     @task()
     def load(data: pd.DataFrame):
-        logging.info(LOG_FORMAT + f'{data.columns.tolist()}')
-        # загружаем данные в целевую таблицу
-        hook = PostgresHook('destination_db')
-        hook.insert_rows(
-            table="wb_statistic_dag",
-            replace=True,
-            target_fields=data.columns.tolist(),
-            replace_index=['project_id'],
-            rows=data.values.tolist()
-        )
-
+        # Your code here
+        pass
+    
     # создаем таблицу
     create_table()
     # экспортируем данные
     data = extract()
     # преобразовываем данные
-    transformed_data = transform(data)
+    # transformed_data = transform(data)
     # загружаем данные в целевую таблицу
-    load(transformed_data)
+    # load(transformed_data)
 
-prepare_wb_data()
 
 if __name__ == '__main__':
     prepare_wb_data()
